36d7833d4713f7f0ae450325078a1d285a605c1a
Cristian Camilo Riveros Torres <ccriverost@unal.edu.co>
2017-08-27 18:22:07 -0500

    update

--- Overflow.cpp
+++ Overflow.cpp
@@ -5,20 +5,16 @@ using namespace std;
 
 float under = 1.0;
 float over = 1.0;
-int N = 100;
+int N = 200;
 
-int main(int argc, char **argv)
+int main()
 {
-
-  int N = std::atoi(argv[1]);
-  float under
-  cout << "N" << "\t" << "under" << "\t" << "over" <<endl;
   for(int i =1; i < N; i++)
-  {
-    under = under/2;
-    over = over*2;
-    
-    cout << i << "\t" << under << "\t" << over <<endl;
-  } 
+    {
+      under = under/2;
+      over = over*2;
+      
+      cout << i << "\t" << under << "\t"  << over <<endl;
+    } 
   return 0;
 }
